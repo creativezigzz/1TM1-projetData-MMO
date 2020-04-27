13907023c01738dd0dc78e8c781f210e0b2ae320
Lucas Silva <he201892@students.ephec.be>
2020-04-27 17:08:04 +0200

    genre + data table + Utils.js

---
+++ backend/data_tables.sql
@@ -0,0 +1,63 @@
+insert into genre
+(genrId,genrNom)
+values
+(1,"Action"),
+(2,"Aventure"),
+(3,"Comédie"),
+(4,"Drama"),
+(5,"Slice of Life"),
+(6,"Fantaisie"),
+(7,"Horreur"),
+(8,"Mystères")
+(9, "Psychologique"),
+(10,"Romance"),
+(11,"Science-Fiction"),
+(12,"Arts martiaux"),
+(13,"Furyo");
+
+insert into personne
+(pseudo,nomP,prenomP,mdp,token)
+values
+("igzz","Silva","Lucas","kikou"),
+("matWolve","Walravens","Mathieu","koko"),
+("qServais","Servais","Quentin","kiki"),
+("slime789","Grandjean","Cyril","kuku");
+
+insert into anime
+(titre,genrId)
+values
+("Full Metal Alchimist: Brotherhood",2),
+("Cowboy Bebop",11),
+("Code Geass",9),
+("Neon Genenis Evangelion",11),
+("Dragon Ball Z",1),
+("Naruto Shippuden",2),
+("Samurai Champloo",12),
+("Détective Conan",8),
+("One Piece",2),
+("Hunter x Hunter",2),
+("Attack on Titan",1),
+("Jojo's Bizarre Adventure",2),
+("Sword Art Online",6);
+
+insert into myList
+(pseudo,animeId,rating)
+values
+("igzz",2,5),
+("igzz",4,5),
+("qServais",2,4),
+("qServais",10,5),
+("slime789",10,4),
+("slime789",1,4),
+("slime789",5,1),
+("slime789",6,2),
+("slime789",8,3),
+("matWolve",8,5),
+("matWolve",6,5),
+("matWolve",2,5),
+("matWolve",1,5);
+
+
+
+
+
