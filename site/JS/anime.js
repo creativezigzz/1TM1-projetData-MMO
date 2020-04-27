db9a0accbb6144018b4f3aa19ed3313e84b355b4
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-04-27 17:17:00 +0200

    form + anime
    modification de form les genres seront dans un select
    anime: nouvelle fonction pour les genre mais il faut mettre le url dans la requette

--- site/JS/anime.js
+++ site/JS/anime.js
@@ -1,1 +1,18 @@
 "use strict";
+
+/**
+ * cette fonction permet de cree le select des genre dans le html.
+ */
+function initGenre(){
+    let xhr = new XMLHttpRequest();
+    xhr.open('get',"");
+    xhr.onload = function reponse(){
+        let obj = JSON.parse(xhr.responseText);
+        let stringHtlm = "";
+        for(let i of obj){
+            stringHtlm += `<option value="${i.genrId}">${i.genrNom}</option>`;
+        }
+        //document.getElementById().
+    };
+    xhr.send();
+}
