3cb93f26f25ab46fd2090d28ed972385a2141f65
Mathieu Walravens <m.walravens@students.ephec.be>
2020-05-01 18:22:04 +0200

    Ajout de la procédure http_getPage

--- backend/webservices-procedures-base.sql
+++ backend/webservices-procedures-base.sql
@@ -33,3 +33,38 @@ BEGIN
 	END IF;
 	RETURN RIGHT(filename, @pos - 1); /* On retourne les X derniers caractères */
 END;
+
+
+CREATE PROCEDURE http_getPage(in url char(255))
+/*
+	Récupère le contenu d'un fichier tout en mettant l'en-tête 'Content-Type'
+	associée à l'extension du fichier (html par défaut).
+*/
+BEGIN
+	DECLARE @ext varchar(5);
+	DECLARE @file long varchar;
+
+	SET @ext = getExtension(url); /* On récupère l'extension du fichier */
+	IF @ext is NULL THEN /* Si elle n'existe pas, on suppose que c'est un fichier html */
+		SET url = url || '.html';
+		SET @ext = 'html';
+	END IF;
+	CALL sa_set_http_header('Content-Type',
+		CASE /* On utilise le mimetype associé à l'extension */
+			WHEN @ext = 'html' THEN 'text/html; charset=utf-8'
+			WHEN @ext = 'js' THEN 'application/javascript'
+			WHEN @ext = 'css' THEN 'text/css'
+			WHEN @ext = 'png' THEN 'image/png'
+		END
+	);
+
+	/* On récupère le contenu du fichier */
+	SET @file = xp_read_file(dba.getPath() || '..\\site\\' || url);
+
+	IF @file is NULL THEN /* Si le fichier n'existe pas, il est NULL */
+		/* Donc on retourne le code d'erreur 404 (NotFound) */
+		CALL sa_set_http_header('@HttpStatus', '404');
+	END IF;
+
+	SELECT @file; /* On retourne le fichier demandé */
+END;
