053bb7c2e1bbd20667df29ed7f2cdf5ee472b14d
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-05-02 16:10:25 +0200

    mise a jour du html et js
    mise a jour du html (lier les page js)
    modification du js rajout de fonction

--- site/JS/anime.js
+++ site/JS/anime.js
@@ -3,16 +3,15 @@
 /**
  * cette fonction permet de cree le select des genre dans le html.
  */
-function initGenre(){
-    let xhr = new XMLHttpRequest();
-    xhr.open('get',"");
-    xhr.onload = function reponse(){
-        let obj = JSON.parse(xhr.responseText);
-        let stringHtlm = "";
-        for(let i of obj){
-            stringHtlm += `<option value="${i.genrId}">${i.genrNom}</option>`;
-        }
-        //document.getElementById().
-    };
-    xhr.send();
+
+function initForm(){
+    xhrReqJson('http://localhost/getGenrList',reponse,'genre');
+}
+
+function reponse(obj,id){
+    let stringHtlm = "";
+    for(let i of obj){
+        stringHtlm += `<option value="${i.genrId}">${i.genrNom}</option>`;
+    }
+    setElem(id,stringHtlm);
 }
