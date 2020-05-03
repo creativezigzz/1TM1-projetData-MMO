3a2f9d0a1ab3e1a18f4f1b64e9f724add4d784fd
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-05-03 11:08:38 +0200

    modif js et form html
    le select qui se contruit dynamiquement fonctionne

--- site/JS/anime.js
+++ site/JS/anime.js
@@ -1,17 +1,22 @@
 "use strict";
 
+
+/**
+ * fonction d'initialisation de la page form.html
+ */
+function initForm(){
+    xhrReqJson('http://localhost/getGenrList/',reponse,'genre');
+}
 /**
  * cette fonction permet de cree le select des genre dans le html.
+ *
+ * @obj {array} objet qui sera utiliser pour former le select
+ * @id {string} id de la page html ou les données iront
  */
-
-function initForm(){
-    xhrReqJson('http://localhost/getGenrList',reponse,'genre');
-}
-
 function reponse(obj,id){
     let stringHtlm = "";
     for(let i of obj){
-        stringHtlm += `<option value="${i.genrId}">${i.genrNom}</option>`;
+        stringHtlm += `<option value="${i.id}">${i.genre}</option>`;// creation des option
     }
     setElem(id,stringHtlm);
 }
