0e75e76213e0a50170d8853402c75abbca1864eb
QuentinServais <64021670+QuentinServais@users.noreply.github.com>
2020-05-04 18:11:40 +0200

    js

---
+++ site/JS/myAnimeList.js
@@ -0,0 +1,32 @@
+var box = document.getElementById("liste");
+var ourRequest = new XMLHttpRequest();
+
+ourRequest.open('GET', 'http://localhost/getAnimeList');
+ourRequest.onload = function (){
+	var ourData = JSON.parse(ourRequest.responseText);
+	renderHTML(ourData);
+};
+ourRequest.send();
+
+function renderHTML(data){
+	var str = "";
+	var cols = Object.keys(data[0])
+	var table = "<table>"
+	var titre = "<tr>"
+	for(var i in cols){
+	titre += "<th>" + cols[i] + "</th>"
+  }
+  titre += "</tr>"
+
+	var liste = ""
+	for(var i in data){
+	liste += "<tr>"
+	liste += "<td>" + data[i].animeNom + "</td>"
+	liste += "<td>" + data[i].note + "</td>"
+	liste += "</tr>"
+  }
+	
+  var table = "<table>" + titre + liste + "</table>"	
+  document.getElementById("liste").innerHTML = table;
+  console.log(cols);
+}
