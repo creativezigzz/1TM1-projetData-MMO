3fd6f53a229e43e9c7ea4ce0f8304b4cde873f5f
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-05-05 18:14:19 +0200

    modif html et mon js

--- site/JS/cyrilGrandjean.js
+++ site/JS/cyrilGrandjean.js
@@ -3,10 +3,10 @@
  * fonction d'initialisation de la page form.html
  */
 function initForm(){
-    xhrReqJson('http://localhost/getAllAnime/',titreSelect,'titre');
+    xhrReqJson('/getAllAnime',titreSelect,'titre');
 }
 function initAjout(){
-    xhrReqJson('http://localhost/getGenrList/',genreSelect,'genre');
+    xhrReqJson('/getGenrList',genreSelect,'genre');
 }
 /**
  * cette fonction permet de créer le select des genres dans le html.
@@ -21,7 +21,12 @@ function titreSelect(obj,id){
     }
     setElem(id,stringHtml);
 }
-
+/**
+ * cette fonction permet de créer le select des genres dans le html.
+ *
+ * @obj {array} objet qui sera utilisé pour former le select
+ * @id {string} id de la page html ou les données iront
+ */
 function genreSelect(obj,id){
     let stringHtml = "";
     for(let i of obj){
