e84f94e75a2752103728f58f449f2b4c32876f08
QuentinServais <64021670+QuentinServais@users.noreply.github.com>
2020-05-05 18:02:18 +0200

    jsQuentin

--- site/JS/quentinServais.js
+++ site/JS/quentinServais.js
@@ -1,33 +1,33 @@
 var box = document.getElementById("liste");
-var ourRequest = new XMLHttpRequest();
 
 function initListe(){
-ourRequest.open('GET', 'http://localhost/getAnimeList');
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'http://localhost/getAnimeList?token=' +);
 ourRequest.onload = function (){
 	var ourData = JSON.parse(ourRequest.responseText);
 	renderHTML(ourData);
 };
 ourRequest.send();
 }
+
+
 function renderHTML(data){
-	var str = "";
-	var cols = Object.keys(data[0]);
-	var table = "<table>";
+	var cols = Object.keys(data[0])
 	var titre = "<tr>";
 	for(var i in cols){
 	titre += "<th>" + cols[i] + "</th>"
   }
-  titre += "</tr>";
+  titre += "</tr>"
 
 	var liste = "";
 	for(var i in data){
-	liste += "<tr>";
-	liste += "<td>" + data[i].animeNom + "</td>";
-	liste += "<td>" + data[i].note + "</td>";
-	liste += "</tr>";
+	liste += "<tr>"
+	liste += "<td>" + data[i].animeNom + "</td>"
+	liste += "<td>" + data[i].genrNom + "</td>"
+	liste += "<td>" + data[i].note + "</td>"
+	liste += "</tr>"
   }
 	
-  var table = "<table>" + titre + liste + "</table>";
-  document.getElementById("liste").innerHTML = table;
-  console.log(cols);
+  var table = "<table>" + titre + liste + "</table>"	
+  box.innerHTML = table;
 }
