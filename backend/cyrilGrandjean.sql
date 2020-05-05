f022db61779ccbd7f22a3b9f15b8275595376b01
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-05-05 12:55:47 +0200

    modif fichier sql

---
+++ backend/cyrilGrandjean.sql
@@ -0,0 +1,40 @@
+CREATE PROCEDURE get_animeListRatio()
+result(titre char(256),genre char (256), note int)
+BEGIN 
+    select an.titre ,gr.genrNom, sum(li.rating)/count(li.pseudo) from myList as li
+    natural join anime as an
+    natural join genre as gr
+    group by titre, li.animeId,gr.genrNom
+    order by titre asc
+end;
+
+/*
+creation du service qui renvoie tout les anime existant dans la base de donée
+ et leur ratio dans un objet json.
+*/
+create service "getAnimeListRatio"
+    type 'JSON'
+    authorization off
+    methods 'get'
+    user "DBA"
+    url on
+as call get_animeListRatio()
+
+create procedure get_genreList()/*creation de la procedure get_genreList*/
+result( id integer, genre char(30))/*renvoie un id qui est un nombre et le genre qui est un charactere*/
+begin
+    select * from dba.genre/*selectionne toutes les données de la table genre*/
+    order by genrNom ASC /*les trie sur les noms par ordre croissant */
+end;
+
+/*
+creation du service qui renvoie tout les genre et leur id
+dans un objet json.
+*/
+create service "getGenrList"
+    type 'JSON'
+    authorization off
+    methods 'get'
+    user "DBA"
+    url on
+as call get_genreList()
