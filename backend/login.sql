45b3769db1adbf91a163e546c1ca28633ed84796
Mathieu Walravens <m.walravens@students.ephec.be>
2020-05-05 16:11:01 +0200

    Ajout fonction createToken
    Crée un token et vérifie qu'il n'existe pas encore dans la table personne.

--- backend/login.sql
+++ backend/login.sql
@@ -1,3 +1,19 @@
+CREATE FUNCTION createToken()
+/* Génère un nouveau token unique à l'aide des fonctions rand() et hash() */
+RETURNS char(32)
+BEGIN
+	DECLARE @token char(32);
+	SET @token = hash(rand(), 'md5');
+
+	/* On s'assure que le token est unique */
+	IF (SELECT 1 FROM personne WHERE token = @token) = 1 THEN
+		/* Sinon on le re génère */
+		SET @token = createToken();
+	ENDIF;
+
+	RETURN @token;
+END;
+
 CREATE PROCEDURE "DBA"."login"( IN username char(30), IN password char(64) )
 RESULT( nom char(30), prenom char(30), token char(32) )
 BEGIN
@@ -5,7 +21,7 @@ BEGIN
 	IF (SELECT 1 FROM personne WHERE pseudo = username AND mdp = password) = 1 THEN
 		/* Oui, on crée un nouveau token (max 1 session) */
 		UPDATE personne
-		SET token = hash(rand(), 'md5')
+		SET token = createToken()
 		WHERE pseudo = username;
 
 		/* Et on retourne les infos */
@@ -24,17 +40,21 @@ CREATE SERVICE "login"
 AS call "DBA"."login"(:username, :mdp);
 
 CREATE PROCEDURE "DBA"."add_user"( IN username char(30), IN nom char(30), IN prenom char(30), IN @mdp char(64) )
+/* Ajoute un nouvel utilisateur dans la base de donnée */
 RESULT( success BOOLEAN, "message" char(60), token char(32)  )
 BEGIN
 	DECLARE @token char(32);
 
+	/* On vérifie que le pseudo est unique. */
 	IF (SELECT 1 FROM personne WHERE pseudo = username) = 1 THEN
 		SELECT 0, 'Ce pseudo n''est pas disponible.', NULL;
 		RETURN;
 	ENDIF;
 
-	SET @token = hash(rand(), 'md5');
+	/* On crée un nouvel token */
+	SET @token = createToken();
 
+	/* On ajoute la personne à la table personne */
 	INSERT INTO personne VALUES (
 		username,
 		nom,
@@ -43,6 +63,7 @@ BEGIN
 		@token
 	);
 
+	/* Tout s'est bien déroulé, on retourne le token */
 	SELECT 1, NULL, @token;
 END;
 
