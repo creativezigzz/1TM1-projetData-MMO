889e6f9ab081b32daeaf17f96f45b4db2e3e18fa
Lucas Silva <he201892@students.ephec.be>
2020-05-06 15:47:36 +0200

    Update lucasSilva.sql

--- backend/lucasSilva.sql
+++ backend/lucasSilva.sql
@@ -5,21 +5,17 @@ RESULT (success BOOLEAN)
 BEGIN
   Call sa_set_http_header('Access-Control-Allow-Origin', '*');
   /*On s'assure que la personne est connectée*/
-  IF((SELECT 1 FROM personne WHERE token = @token ) = 1) THEN
+  IF((SELECT 1 FROM personne WHERE token = @token) = 1 OR (SELECT 1 from myList WHERE myList.animeId=animei) = 1) THEN
     /*On regarde si l'anime n'est pas déjà dans la liste*/
-    IF((SELECT 1 from myList WHERE myList.animeId=animei) = 1 )THEN
     SELECT 0;
-    ELSE
-      /*On rajoute les donnnées dans la table personnelle;*/
-      INSERT INTO myList VALUES (
-        (SELECT pseudo from personne where token = @token),
-        animei,
-        note
-      );
-      SELECT 1;
-    ENDIF;
-  ELSE SELECT 0;
+    RETURN;
   ENDIF;
+  INSERT INTO myList VALUES (
+    (SELECT pseudo from personne where token = @token),
+    animei,
+    note
+  );
+  SELECT 1;
 END;
 
 CREATE SERVICE "add_mylist"
@@ -30,23 +26,23 @@ CREATE SERVICE "add_mylist"
   METHODS 'POST,GET'
 AS call "DBA"."add_mylist"(:token,:titre,:note);
 
-CREATE PROCEDURE "DBA"."add_anime" (IN titre char(60) ,IN genre char(30))
+CREATE PROCEDURE "DBA"."add_anime" (IN @titre char(60) ,IN genre char(30))
 RESULT (success BOOLEAN)
 BEGIN
 /*On regarde d'abord si l'anim est dans la table */
   DECLARE gr integer;
-  IF ((SELECT 1 from anime natural join genre WHERE titre = titre AND  genre.genreNom =genre) = 1) THEN
+  IF ((SELECT 1 from anime natural join genre WHERE anime.titre = @titre AND  genre.genrNom =genre) = 1) THEN
   SELECT 0;
   /*Sinon juste on continue*/
   ELSE
 
-    SET gr = (select genreId from genre where genre.genreNom = genre);
+    SET gr = (select genreId from genre where genre.genrNom = genre);
     INSERT INTO anime (
       titre,
       genrId
     )
     VALUES(
-      titre,
+      @titre,
       gr
     );
 
