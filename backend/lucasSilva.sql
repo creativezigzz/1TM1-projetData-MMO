779565090b4e0eb111f00426b8bd690febd3ad5e
Lucas Silva <he201892@students.ephec.be>
2020-05-06 14:44:11 +0200

    réglages problemes

--- backend/lucasSilva.sql
+++ backend/lucasSilva.sql
@@ -1,66 +1,24 @@
 CREATE PROCEDURE "DBA"."add_mylist" (IN @token char(32), IN titre char(60),IN note tinyint)
 
 /*Rajouter un nouvel anime dans sa liste personnel*/
-RESULT (succes BOOLEAN)
+RESULT (success BOOLEAN)
 BEGIN
-  DECLARE pseud CHAR(60);
-  DECLARE animi INTEGER;
-  /*On s'assure que la personne est connect√©e*/
-  IF(SELECT 1 FROM personne WHERE token = @token ) = 1 THEN
-    /*On regarde si l'anime n'est pas d√©j√† dans la liste*/
-    IF(SELECT 1 from myList natural join anime WHERE anime.titre=titre) THEN
+  Call sa_set_http_header('Access-Control-Allow-Origin', '*');
+
+  /*On s'assure que la personne est connectÈe*/
+  IF((SELECT 1 FROM personne WHERE token = @token ) = 1) THEN
+    /*On regarde si l'anime n'est pas dÈj‡ dans la liste*/
+    IF((SELECT 1 from myList natural join anime WHERE anime.titre=titre) = 1 )THEN
     SELECT 0;
     ELSE
-      SET pseud = (SELECT pseudo from personne where token = @token);
-      SET animi = (SELECT animId from anime where anime.titre= titre);
-      /*On rajoute les donnn√©es dans la table personnelle;*/
+      /*On rajoute les donnnÈes dans la table personnelle;*/
       INSERT INTO myList VALUES (
-        pseud,
-        animi,
+        (SELECT pseudo from personne where token = @token),
+        (SELECT animId from anime where anime.titre= titre),
         note
       );
       SELECT 1;
-    ENDIF
+    ENDIF;
   ELSE SELECT 0;
-  ENDIF
-END;
-
-CREATE SERVICE "add_mylist"
-  TYPE 'JSON'
-  AUTHORIZATION OFF
-  USER "DBA"
-  URL ON
-  METHODS 'POST,GET'
-AS call "DBA"."add_mylist"(:token,:titre,:note);
-
-CREATE PROCEDURE "DBA"."add_anime" (IN titre char(60) ,IN genre char(30))
-RESULT (success BOOLEAN)
-BEGIN
-/*On regarde d'abord si l'anim est dans la table */
-  DECLARE gr integer;
-  IF (SELECT 1 from anime natural join genre WHERE titre = titre AND  genre.genreNom =genre) = 1 THEN
-  SELECT 0;
-  /*Sinon juste on continue*/
-  ELSE
-
-    SET gr = (select genreId from genre where genre.genreNom = genre);
-    INSERT INTO anime (
-      titre,
-      genrId
-    ) ;
-    VALUES(
-      titre,
-      gr
-    );
-
-    SELECT 1;
   ENDIF;
-END
-
-CREATE SERVICE "add_anime"
-  TYPE 'JSON'
-  AUTHORIZATION OFF
-  USER "DBA"
-  URL ON
-  METHODS 'POST,GET'
-AS call "DBA"."add_anime"(:titre,:genre);
+END;
