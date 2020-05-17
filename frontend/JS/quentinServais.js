975524b59d1b5cd4137b2e183c201ffc15d21b29
Quentins <quentin.f.servais@student.uclouvain.be>
2020-05-17 17:57:55 +0200

    doc

--- frontend/JS/quentinServais.js
+++ frontend/JS/quentinServais.js
@@ -4,13 +4,13 @@
  * @author Quentin Servais HE201860
 */
 function initListe(){
-var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', '/getAnimeList?token=' + getCookie("token"));
-ourRequest.onload = function (){
-	var ourData = JSON.parse(ourRequest.responseText);
-	renderHTML(ourData);
-};
-ourRequest.send();
+	var ourRequest = new XMLHttpRequest();
+	ourRequest.open('GET', '/getAnimeList?token=' + getCookie("token"));
+	ourRequest.onload = function (){
+		var ourData = JSON.parse(ourRequest.responseText);
+		renderHTML(ourData);
+	};
+	ourRequest.send();
 }
 
 /**
@@ -24,49 +24,67 @@ function renderHTML(data){
 	var cols = Object.keys(data[0]);
 	var titre = "<tr>";
 	for(var i in cols){
-	titre += "<th>" + cols[i] + "</th>"; //mise en place du Thead
+		titre += "<th>" + cols[i] + "</th>"; //mise en place du Thead
   }
   titre += "</tr>";
 
 	var liste = "";
 	for(var i in data){
-	liste += "<tr>"; //mise en place du Tbody
-	liste += "<td>" + data[i].anime + "</td>";
-	liste += "<td>" + data[i].note + "</td>";
-	liste += "<td>" + data[i].genre + "</td>";
-	liste += "</tr>"
-  }
+		liste += "<tr>"; //mise en place du Tbody
+		liste += "<td>" + data[i].anime + "</td>";
+		liste += "<td>" + data[i].note + "</td>";
+		liste += "<td>" + data[i].genre + "</td>";
+		liste += "</tr>"
+	}
 
   var table = "<table>" + titre + liste + "</table>";
   box.innerHTML = table; //position du tableau dans le html
 }
-
+/**
+ * fonction d'initialisation ajax pour préparer le formulaire de suppression
+ * @author Quentin Servais
+*/
 function initSuppr(){
-var suppRequest = new XMLHttpRequest();
-suppRequest.open('GET', '/getTitre?token=' + getCookie("token"));
-suppRequest.onload = function (){
-var suppr = JSON.parse(suppRequest.responseText);
-addSelect(suppr);
-};
-suppRequest.send();
+	var suppRequest = new XMLHttpRequest();
+	suppRequest.open('GET', '/getTitre?token=' + getCookie("token"));
+	suppRequest.onload = function (){
+		var suppr = JSON.parse(suppRequest.responseText);
+		addSelect(suppr);
+	};
+	suppRequest.send();
 }
+/**
+ * fonction d'initialisation du formulaire de suppression
+ * @author Quentin Servais
 
+*/
 function addField(){
     var container = document.getElementById("field");
-    container.innerHTML =     '<form onsubmit="suppression(this);"><label for="selectTitre">Anime à supprimer:</label><br><select id="selectTitre" name="select"></select><br><input type="submit" value="Submit"></form>';
+  	container.innerHTML = '<form onsubmit="suppression(this);"><label for="selectTitre">Anime à supprimer:</label><br><select id="selectTitre" name="select"></select><br><input type="submit" value="Submit"></form>';
 		initSuppr();
 }
+/**
+ * fonction d'initialisation ajax pour préparer le formulaire de suppression
+ * @author Quentin Servais
 
+ * @param {HTMLFormElement} form : le formaulaire
+*/
 function suppression(form){
 	var supprime = new XMLHttpRequest();
-	supprime.open('GET', '/remove?id=' + form.select +'&token=' + getCookie("token"));
+	supprime.open('GET', '/remove?id=' + form.select.value +'&token=' + getCookie("token"));
 	supprime.onload = function(){
-	var del	= supprime.responseText;
-	document.getElementById("field").innerHTML = del;
-};
-supprime.send();
+		var del	= supprime.responseText;
+		document.getElementById("field").innerHTML = del;
+		initListe();
+	};
+	supprime.send();
 }
+/**
+ * fonction qui créé les options de la liste déroulante de suppression
+ * @author Quentin Servais
 
+ * @param {object} obj : objet de la requête
+*/
 function addSelect(obj){
 	var container = document.getElementById("selectTitre");
 	var strHTML = "";
